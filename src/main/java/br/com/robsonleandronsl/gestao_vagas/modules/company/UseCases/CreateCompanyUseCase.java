124ed3efb319bc41333a44f2a810b5bf6e3d503f
robson <devrobsoncastro@hotmail.com>
2024-02-28 13:04:22 -0300

    created commit git

---
+++ src/main/java/br/com/robsonleandronsl/gestao_vagas/modules/company/UseCases/CreateCompanyUseCase.java
@@ -0,0 +1,26 @@
+package br.com.robsonleandronsl.gestao_vagas.modules.company.UseCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.robsonleandronsl.gestao_vagas.exceptions.UserFoundException;
+import br.com.robsonleandronsl.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.robsonleandronsl.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class CreateCompanyUseCase {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyEntity execute(CompanyEntity companyEntity) {
+
+        this.companyRepository
+                .findByusernameOrEmail(companyEntity.getName(), companyEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.companyRepository.save(companyEntity);
+    }
+}
