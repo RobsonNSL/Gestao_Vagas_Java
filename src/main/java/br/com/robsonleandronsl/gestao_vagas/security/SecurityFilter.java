de261a7b31203a76b4e4001324ea66342a730761
robson <devrobsoncastro@hotmail.com>
2024-03-05 17:21:05 -0300

    Update

--- src/main/java/br/com/robsonleandronsl/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/robsonleandronsl/gestao_vagas/security/SecurityFilter.java
@@ -2,9 +2,11 @@ package br.com.robsonleandronsl.gestao_vagas.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.robsonleandronsl.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,14 +15,21 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JWTProvider jmwProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         String header = request.getHeader("Authorization");
 
-        if(header != null) {
-            
+        if (header != null) {
+            var subjectToken = this.jmwProvider.validateToken(header);
+            if (subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
         }
 
         filterChain.doFilter(request, response);
