cdd20ad931356d60cd695f4fe1154087a5a7972d
robson <devrobsoncastro@hotmail.com>
2024-03-07 12:18:43 -0300

    Update

---
+++ src/main/java/br/com/robsonleandronsl/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,5 @@
+package br.com.robsonleandronsl.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+    
+}
--- src/main/java/br/com/robsonleandronsl/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/robsonleandronsl/gestao_vagas/security/SecurityConfig.java
@@ -21,11 +21,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/candidate/").permitAll()
                             .requestMatchers("/company/").permitAll()
-                            .requestMatchers("/auth/company").permitAll();
+                            .requestMatchers("/auth/company").permitAll()
+                            .requestMatchers("/candidate/auth").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
-                ;
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
